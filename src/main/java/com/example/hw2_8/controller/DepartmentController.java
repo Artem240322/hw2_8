a047660b16866349f3c2345bdd00be56d95f5b79
ArtemCH240322 <sun2383@mail.ru>
2022-07-05 15:29:13 +0300

    Первый комит. Задание 1 ввел отлел и прочее. проект скопировал. Задание 2. Задание 3.

---
+++ src/main/java/com/example/hw2_8/controller/DepartmentController.java
@@ -0,0 +1,46 @@
+package com.example.hw2_8.controller;
+
+
+import com.example.hw2_8.model.Employee;
+import com.example.hw2_8.service.DepartmentService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/departments")
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @GetMapping("/max-salary")
+    public Employee findEmployeeWithMaxSalaryFromDepartment(@RequestParam("departmentId") int department) {
+        return DepartmentService.findEmployeeWithMaxSalaryFromDepartment(department);
+
+    }
+    @GetMapping("/min-salary")
+    public Employee findEmployeeWithMaxSalaryFromDepartment(@RequestParam("departmentId") int department) {
+        return DepartmentService.findEmployeeWithMaxSalaryFromDepartment(department);
+
+    }
+    @GetMapping(value = "/all", params = "departmentId")
+    public List<Employee> findEmployeeFromDepartment(@RequestParam("departmentId") int department) {
+        return DepartmentService.findEmployeeFromDepartment(department);
+
+    }
+
+    @GetMapping("/all")
+    public Map<Integer, List<Employee>> findEmployee() {
+        return DepartmentService.findEmployee();
+
+    }
+
+}
---
+++ src/main/java/com/example/hw2_8/service/DepartmentService.java
@@ -0,0 +1,33 @@
+package com.example.hw2_8.service;
+
+import com.example.hw2_8.model.Employee;
+
+import java.util.List;
+import java.util.Map;
+
+public class DepartmentService {
+
+    private final EmployeeService employeeService;
+
+    public DepartmentService(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+
+    public Employee findEmployeeWithMaxSalaryFromDepartment(int department) {
+        return EmployeeService.getAll.stream()
+    }
+
+    public Employee findEmployeeWithMaxSalaryFromDepartment(int department) {
+        return null;
+    }
+    public static List<Employee> findEmployeeFromDepartment(int department) {
+        return null;
+    }
+
+    public static Map<Integer, List<Employee>> findEmployee() {
+
+    }
+
+
+}
